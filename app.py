1e20ff7608e9b4f4cdc14021fa0809482a4086eb
VerifiedFans <troy@knoxconcepts.com>
2025-02-05 13:37:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,6 @@ def download_file():
     return send_file(geojson_file, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Ensure it runs on Heroku
+    app.run(debug=True, host="0.0.0.0", port=port)
+
