f92654ac31eed2b42c58c1cb89148aa56d42b0e0
VerifiedFans <troy@knoxconcepts.com>
2025-02-13 23:37:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -103,6 +103,6 @@ def download_geojson():
         return send_file(geojson_path, as_attachment=True)
     return jsonify({"message": "GeoJSON file not found"}), 404
 
-# Run the app
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
